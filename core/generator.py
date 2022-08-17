efaf42c37288fd836596e618598ca3584a42664a
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-08-17 08:09:14 +0430

    fix: change body holds logic

--- core/generator.py
+++ core/generator.py
@@ -11,7 +11,7 @@ class Generator:
         self.map = Map()
 
     def _length_(self):
-        self.length = random.randint(3, 10)
+        self.length = random.randint(3, 11)
 
     def _start_(self):
         size = random.randint(1, 2)
@@ -26,18 +26,6 @@ class Generator:
             if int(hold[0]) > int(start_limit):
                 start_limit = hold[0]
         return start_limit
-
-    def _body_(self):
-        self._length_()
-        self._start_()
-        self._top_()
-        start = self._start_limit_()
-        for i in range(1, int(self.length)):
-            self.body.append(f'{random.randint(int(start), 17)}{random.choice(self.map.letters)}')
-
-    def get_body(self):
-        self._body_()
-        return self.body
 
     def alphabet_to_number(self, hold) -> (int):
         if hold.endswith('A'):
@@ -65,9 +53,45 @@ class Generator:
 
     def get_row_of_hold(self, hold):
         if len(hold) == 3:
-            return hold[0] + hold[1]
+            return int(hold[0] + hold[1])
         if len(hold) == 2:
-            return hold[0]
+            return int(hold[0])
+
+    def get_start_max(self):
+        if len(self.start) == 2:
+            return max(self.get_row_of_hold(self.start[0]), self.get_row_of_hold(self.start[1]))
+        return self.get_row_of_hold(self.start[0])
+
+    # TODO read path
+    def get_next_hold(self):
+        pre_hold = '1A'
+        while self.get_row_of_hold(pre_hold) < 17:
+            if len(self.body) == 0:
+                # print("start_limit: " + str(self.get_start_max()))
+                self.body.append(
+                    f'{str(random.randint(self.get_start_max(), self.get_start_max() + 6))}{random.choice(self.map.letters)}')
+            else:
+                pre_hold = self.body[len(self.body) - 1]
+                # print("pre_hold: " + pre_hold)
+                if self.get_row_of_hold(pre_hold) + 6 >= 18:
+                    end = 17
+                else:
+                    end = self.get_row_of_hold(pre_hold) + 6
+                self.body.append(
+                    f'{random.randint(self.get_row_of_hold(pre_hold), end)}{random.choice(self.map.letters)}')
+
+    def _body_(self):
+        self._length_()
+        self._start_()
+        self._top_()
+        # start = self._start_limit_()
+        self.get_next_hold()
+        # for i in range(1, int(self.length)):
+        #     self.body.append(f'{random.randint(int(start), 17)}{random.choice(self.map.letters)}')
+
+    def get_body(self):
+        self._body_()
+        return self.body
 
     def draw_board(self):
         start_color = '\033[92m'
