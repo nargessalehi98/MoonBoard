ef37794054a0828af7a1db56469bdcc4f5fb6b51
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-08-28 14:20:39 +0430

    feat: add route api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from core.generator import Generator
+from core.result import Result
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+
+
+class Route(APIView):
+
+    def get(self, request):
+        generator = Generator()
+        start = generator.get_start()
+        body = generator.get_body()
+        top = generator.get_top()
+        generator.draw_board()
+        result = Result()
+        output = {
+            "grade": result.get_final_result(start, body + top),
+            "start": start,
+            "body": body,
+            "top": top
+        }
+        return Response(data=output, status=HTTP_200_OK)
 
 # Create your views here.
