202e230e68413aed2dcb61a0b7c032d0bf37b36d
nargessalehi98 <nargessalehi98@aut.ac.ir>
2022-09-04 18:01:41 +0430

    fix: used copy to avoid pass by value

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 from core.generator import Generator
 from core.result import Result
 from rest_framework.views import APIView
@@ -11,13 +13,15 @@ class Route(APIView):
     def get(request):
         generator = Generator()
         start = generator.get_start()
+        temp_start = copy(start)
         body = generator.get_body()
         top = generator.get_top()
-        generator.draw_board()
+        # generator.draw_board()
+        print(start, body, top)
         result = Result()
         output = {
             "grade": result.get_final_result(start, body + top),
-            "start": start,
+            "start": temp_start,
             "body": body,
             "top": top
         }
